1b7e05ce5ceac9898459b43e0c132b35694c6953
Adriana Gutierrez <adriana.gutierrez@ucsp.edu.pe>
2022-09-30 23:01:12 -0500

    feat: creates project

---
+++ Nerdery/NerderyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NerderyApp.swift
+//  Nerdery
+//
+//  Created by Adriana Gutierrez on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct NerderyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
